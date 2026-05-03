405eae20346d7863f330f3e08069d9ea1092d0b2
Aakash Sharma <aakash151556@gmail.com>
2026-05-03 16:34:49 +0530

    new20

--- src/TRC20Transfer.jsx
+++ src/TRC20Transfer.jsx
@@ -97,16 +97,34 @@ const sendUSDT = async () => {
   topic: session.topic,
   chainId: "tron:0x2b6653dc",
   request: {
-    method: "eth_signTransaction",
+    method: "tron_signTransaction",
     params: [tx], // 👈 full transaction object
   },
 });
 
-    console.log("SIGNED TX:", signedTx);
+   // console.log("SIGNED TX:", signedTx);
 
-    const broadcast = await tronWeb.trx.sendRawTransaction(signedTx);
+    //const broadcast = await tronWeb.trx.sendRawTransaction(signedTx);
 
-    console.log("Broadcast:", broadcast);
+  //  console.log("Broadcast:", broadcast);
+
+    const broadcastRes = await fetch("https://api.trongrid.io/wallet/broadcasttransaction", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signedTx),
+      });
+
+      const broadcastData = await broadcastRes.json();
+
+      console.log("Broadcast result:", broadcastData);
+
+      if (broadcastData.result) {
+        alert("✅ Transaction sent!");
+      } else {
+        throw new Error(JSON.stringify(broadcastData));
+      }
 
   } catch (error) {
     console.error("ERROR:", error);
